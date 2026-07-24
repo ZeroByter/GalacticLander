2006383d5d24396ae7fcb4f4481db7fb09f3279d
ZeroByter <romgamesrom@gmail.com>
2026-07-24 13:29:05 +0300

    refactor: Improve ButtonHoverSound logic and structure

--- Assets/Scripts/UI/Audio/ButtonHoverSound.cs
+++ Assets/Scripts/UI/Audio/ButtonHoverSound.cs
@@ -1,9 +1,21 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 [DisallowMultipleComponent]
-public class ButtonHoverSound : MonoBehaviour, IPointerEnterHandler {
-    public void OnPointerEnter(PointerEventData eventData) {
+public class ButtonHoverSound : MonoBehaviour, IPointerEnterHandler
+{
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (button != null && !button.interactable) return;
+
         UISoundEmitter.PlaySound(0);
     }
 }
